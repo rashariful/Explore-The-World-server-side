986087c29ffc84e890079c25d6aea0337f353034
md shariful islam <raislam.shariful@gmail.com>
2022-11-08 18:50:32 +0600

    service id added daynamic way

---
+++ index.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const { MongoClient, ObjectId } = require('mongodb');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config()
+
+// Middle ware
+app.use(cors())
+app.use(express.json())
+
+// make uri for mongodb
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri)
+
+async function dbConnect() {
+    try {
+        await client.connect()
+        console.log('mongo connected');
+
+    } catch (error) {
+        console.log(error.message, error.name);
+
+    }
+}
+dbConnect()
+
+// create the database and collection
+const Services = client.db('ExploreWorld').collection('service')
+
+// service Post using post method in mongodb
+app.post('/service', async (req, res) => {
+    try {
+        const result = await Services.insertOne(req.body)
+        if (result.insertedId) {
+            res.send({
+                success: true,
+                message: `Successfully created the ${req.body.name} with this id ${result.insertedId}`
+            })
+        }
+        else {
+            res.send({
+                success: false,
+                error: "Couldn't create the product"
+            })
+        }
+
+    } catch (error) {
+        res.send({
+            success: false,
+            error: error.message
+        })
+    }
+})
+
+// this is get service function for server side
+app.get('/service', async (req, res) => {
+    try {
+        const cursor = Services.find({})
+        const service = await cursor.toArray()
+        res.send({
+            success: true,
+            data: service
+        })
+
+    } catch (error) {
+        res.send({
+            success: false,
+            error: error.message
+        })
+
+    }
+})
+
+// this is get service id function for server side
+app.get('/service/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const quary = { _id: ObjectId(id) };
+        const service = await Services.findOne(quary)
+        res.send({
+            success: true,
+            data: service
+        })
+
+    } catch (error) {
+        res.send({
+            success: false,
+            error: error.message
+        })
+
+    }
+})
+
+
+
+app.get('/', (req, res) => {
+    res.send('Assignment server up')
+})
+
+app.listen(port, () => {
+    console.log(`server up on: ${port}`);
+})
